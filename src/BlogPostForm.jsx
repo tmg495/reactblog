9da4d4ba005ee2a3e5789ce15bc52d97c168ba9d
tmg495 <thetomgrant@gmail.com>
2025-11-20 15:21:33 -0500

    fixed bugs with calender popup and input field

--- src/BlogPostForm.jsx
+++ src/BlogPostForm.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import styles from './BlogPostForm.module.css';
 import DatePicker from "react-datepicker";
 import SunEditor from "suneditor-react";
 import { Link, useNavigate } from 'react-router'
 import "react-datepicker/dist/react-datepicker.css";
 import "suneditor/dist/css/suneditor.min.css";
+import styles from './BlogPostForm.module.css';
 
 const BlogPostForm = ({ post, onSubmit }) => {
     const [title, setTitle] = useState(post?.title || '');
@@ -13,7 +13,7 @@ const BlogPostForm = ({ post, onSubmit }) => {
     const [date, setDate] = useState(post?.date || '');
     const [errors, setErrors] = useState({});
     const navigate = useNavigate();
-    
+
     const buttonText = post == undefined ? "Post" : "Update"
 
     const handleSubmit = (e) => {
@@ -25,58 +25,65 @@ const BlogPostForm = ({ post, onSubmit }) => {
         if (!date) newErrors.date = 'Enter a date';
         if (Object.keys(newErrors).length > 0) {
             setErrors(newErrors);
-            } else {
-                onSubmit({ title, content, author, date });
-            }
-        };
+        } else {
+            onSubmit({ title, content, author, date });
+        }
+    };
 
     return (
         <form className={styles.blogPostForm} onSubmit={handleSubmit}>
 
-        <div className={styles.formGroup}>
-        <label htmlFor="title">Title: </label>
-        <input
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-        />
-        {errors.title && <p className={styles.error}>{errors.title}</p>}
-        </div>
+            <div className={styles.formGroup}>
+                <label htmlFor="title">Title: </label>
+                <input
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                {errors.title && <p className={styles.error}>{errors.title}</p>}
+            </div>
 
-        <div className={styles.formGroup}>
-        <label htmlFor="author">Author: </label>
-        <input
-            id="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-        />
-        {errors.author && <p className={styles.error}>{errors.author}</p>}
-        </div>
+            <div className={styles.formGroup}>
+                <label htmlFor="author">Author: </label>
+                <input
+                    id="author"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+                {errors.author && <p className={styles.error}>{errors.author}</p>}
+            </div>
 
-        <div className={styles.formGroup}>
-        <label htmlFor="date">Date: </label>
-        <DatePicker
-            selected={date ? new Date(date) : null}
-            onChange={(d) => setDate(d.toISOString().split("T")[0])}
-        />
-        {errors.date && <p className={styles.error}>{errors.date}</p>}
-        </div>
+            <div className={styles.formGroup}>
+                <label htmlFor="date">Date: </label>
+                <DatePicker
+                    id='date'
+                    wrapperClassName={styles.datePicker}
+                    popperPlacement='top-end'
+                    popperClassName={styles.popper}
+                    selected={date ? new Date(date) : null}
+                    onChange={(d) => setDate(d.toISOString().split("T")[0])}
+                />
+                {errors.date && <p className={styles.error}>{errors.date}</p>}
+            </div>
 
-        <div className={styles.formGroup}>
-        <SunEditor 
-            setContents={content} 
-            onChange={setContent} 
-        />
-        {errors.content && <p className={styles.error}>{errors.content}</p>}
-        </div>
+            <div className={styles.formGroup}>
+                <SunEditor
+                    id='editor'
+                    wrapperClassName={styles.editor}
+                    setContents={content}
+                    onChange={setContent}
+                />
+                {errors.content && <p className={styles.error}>{errors.content}</p>}
+            </div>
 
-        <button type="submit" onClick={() => {
-            if (errors.length == 0) {
-                navigate('/')
-            }}
+            <button type="submit" onClick={() => {
+                if (errors.length == 0) {
+                    navigate('/')
+                }
+            }
             }>{buttonText}</button>
-        <br></br>
-        <Link to='/'>Home</Link>
+            <br></br>
+            <Link to='/'>Home</Link>
         </form>
     );
 };
